08155f77d48ca8a2065ee02518bd1e469c58533c
Ayush Somani <46413612+AyushSomani001@users.noreply.github.com>
2021-09-18 21:36:12 +0200

    Update config.py

--- Python/config.py
+++ Python/config.py
@@ -14,15 +14,15 @@
 
 # import "config.py" in "python_lab6.py "
 
-_BATCH_ACCOUNT_NAME = ''  # Your batch account name
-_BATCH_ACCOUNT_KEY = ''  # Your batch account key
-_BATCH_ACCOUNT_URL = ''  # Your batch account URL
-_STORAGE_ACCOUNT_NAME = ''
-_STORAGE_ACCOUNT_KEY = ''
+_BATCH_ACCOUNT_NAME = 'lab6ayush'  # Your batch account name
+_BATCH_ACCOUNT_KEY = 'GNClQJ4x9h4D/Om+SvGm7rXMeyaTE9yQRd6nxeizQzTJG1cXfOHzw+2pjtikjheIlQii3djN8Asm66ph+Smm2w=='  # Your batch account key
+_BATCH_ACCOUNT_URL = 'https://lab6ayush.norwayeast.batch.azure.com'  # Your batch account URL
+_STORAGE_ACCOUNT_NAME = 'lab6ayushstorage'
+_STORAGE_ACCOUNT_KEY = '/g1sXVeXLK98SEMLT5R/yl3LnTAvJC9qClk3wdHsTi6pJOYYwCpLLmhXMAfndRQByVk0egRDpOem52VbOfZJ7A=='
 _STORAGE_ACCOUNT_DOMAIN = 'blob.core.windows.net' # Your storage account blob service domain
 
-_POOL_ID = 'PythonQuickstartPool'  # Your Pool ID
+_POOL_ID = 'lab6pool'  # Your Pool ID
 _POOL_NODE_COUNT = 2  # Pool node count
 _POOL_VM_SIZE = 'STANDARD_DS1_V2'  # VM Type/Size
-_JOB_ID = 'PythonQuickstartJob'  # Job ID
+_JOB_ID = 'lab6job'  # Job ID
 _STANDARD_OUT_FILE_NAME = 'stdout.txt'  # Standard Output file
