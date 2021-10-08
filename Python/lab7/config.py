17241bafa590b994400c8a823c71d23658b2e130
Ayush Somani <46413612+AyushSomani001@users.noreply.github.com>
2021-10-08 22:21:03 +0200

    Update config.py

--- Python/lab7/config.py
+++ Python/lab7/config.py
@@ -1,18 +1,16 @@
-# Global constant variables (Azure Storage account/Batch details)
-
-# import "config.py" in "batch_python_tutorial_ffmpeg.py"
+# import "config.py" in "python_lab7.py"
 
 # Update the Batch and Storage account credential strings below with the values
 # unique to your accounts. These are used when constructing connection strings
 # for the Batch and Storage client objects.
 
-_BATCH_ACCOUNT_NAME = ''
-_BATCH_ACCOUNT_KEY = ''
-_BATCH_ACCOUNT_URL = ''
-_STORAGE_ACCOUNT_NAME = ''
-_STORAGE_ACCOUNT_KEY = ''
-_POOL_ID = 'LinuxFfmpegPool'
+_BATCH_ACCOUNT_NAME = 'lab7ayush'
+_BATCH_ACCOUNT_KEY = 'wFKsF3oX1bH1pxnPq71mf2Zo5rmJKRk24JQ/0aFVuWJT6f//+rHellIWC8oZtN0pg9ieURAcNZ29nzn5xDtm4A=='
+_BATCH_ACCOUNT_URL = 'https://lab7ayush.norwayeast.batch.azure.com'
+_STORAGE_ACCOUNT_NAME = 'lab7ayush'
+_STORAGE_ACCOUNT_KEY = '7aYypzez3dBoYaGvw+4T32RxlZOatVX5R+j5B6ngSWf/aHoraeNf+5miRVlZ2oFe5vdubRR5vLchPk13By4GoA=='
+_POOL_ID = 'lab7Pool'
 _DEDICATED_POOL_NODE_COUNT = 0
 _LOW_PRIORITY_POOL_NODE_COUNT = 5
 _POOL_VM_SIZE = 'STANDARD_A1_v2'
-_JOB_ID = 'LinuxFfmpegJob'
+_JOB_ID = 'lab7job'
